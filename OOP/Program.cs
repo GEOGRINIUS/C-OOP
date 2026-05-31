9f6fb8204b8ac37a97a5134f86a8e8d36a0a226b
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2026-05-31 15:37:53 +0400

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point A = new Point();
+            
+            //A.SetX(2);
+            //A.SetY(3);
+            //Console.WriteLine($"X = {A.GetX()}, Y = {A.GetY()}");
+            A.X = 222;
+            A.Y = 333;
+
+            A.Print();
+
+            Point B = new Point(7, 8);
+            B.Print();
+
+            Point C = A + B;
+            C.Print();
+
+        }
+    }
+}
