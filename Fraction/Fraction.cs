f5306bbccff92f016d2ea376fdfa7a83e5cdc9c2
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2026-06-01 12:30:34 +0400

    Fraction Constructors DONE;

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Fraction
+    {
+        public int Integer { get; set; }
+        public int Numerator { get; set; }
+        int denominator;
+        public int Denominator
+        {
+            get => denominator;
+            set => denominator = value == 0 ? 1 : value;
+        }
+
+        //              Constructors:
+        public Fraction()
+        {
+            this.Integer = 0;
+            this.Numerator = 0;
+            this.Denominator = 1;
+            Console.WriteLine("DefaultConstructor");
+        }
+        public Fraction(int integer)
+        {
+            this.Integer = integer;
+            this.Numerator = 0;
+            this.Denominator = 1;
+            Console.WriteLine($"Single-ArgumentConstructor: {GetHashCode()}");
+        }
+        public Fraction(int numerator, int denominator)
+        {
+            this.Integer = 0;
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+            Console.WriteLine($"Constructor:\t{GetHashCode()}");
+
+        }
+        public Fraction(int integer, int numerator, int denominator)
+        {
+            this.Integer = integer;
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+            Console.WriteLine($"Constructor:\t{GetHashCode()}");
+        }
+
+        //                   Methods:
+        public void Print()
+        {
+            if (Integer != 0) Console.Write(Integer);
+            if (Numerator != 0)
+            {
+                if (Integer != 0) Console.Write("(");
+                Console.Write($"{Numerator}/{Denominator}");
+                if (Integer != 0) Console.Write(")");
+            }
+            else if (Integer == 0) Console.Write(0);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Fraction/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Fraction")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fraction")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36daf731-a79f-48ef-aa2e-8fa9f0110547")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
